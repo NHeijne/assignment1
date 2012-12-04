6c750836e0f63f50950f322b40fb50b57989fbf5
Agnes van Belle <agnesvanbelle@gmail.com>
2012-12-04 22:55:39 +0100

    hffh

--- Project1N_1_2/TreeManager.cpp
+++ Project1N_1_2/TreeManager.cpp
@@ -44,8 +44,7 @@ void TreeManager::debinarize(tree<string>& theTree) {
   while (it != theTree.end()) {  
 
     if ((*it)[(*it).size() - 1] == '@') {//if '@' is at last in the name
-
-      cout << "has @: " << (*it) << endl;
+      //cout << "has @: " << (*it) << endl;
 
       tree<string>::iterator thisAt = it;
 
@@ -75,33 +74,35 @@ void TreeManager::debinarize(tree<string>& theTree) {
 void TreeManager::removeSpecialUnaryRules(tree<string>& theTree) {
   int sizeSpecialUnarySymbol = Grammar::specialUnarySymbol.size();
 
+  // assume nt_A%%%%%B
   tree<string>::iterator it = theTree.begin();
   while (it != theTree.end()) {
 
-    int pos = (*it).find(Grammar::specialUnarySymbol); //   position of the first occurrence in the string of the searched content.
+    int pos = (*it).find(Grammar::specialUnarySymbol); //   position of the first occurrence in the string of the searched content
     if (!(pos == string::npos)) { // found
       cout << (*it) << " has " << Grammar::specialUnarySymbol << endl;
 
+      // retrieve A and B from nt_A%%%%%B
       string str1 =  (*it).substr(0, pos);
       string str2 = Grammar::nonTerminalSymbol + (*it).substr(pos+sizeSpecialUnarySymbol, string::npos);
-      cout << "str1: " << str1 << ", " << "str2: " << str2 << endl;
+      //cout << "str1: " << str1 << ", " << "str2: " << str2 << endl;
 
-
+      // Make tree with root nt_B. nt_B's children will be nt_A%%%%%B's children.
       tree<string> tree2(it);
       tree<string>::iterator top2 = tree2.begin();
-      (*top2) = str2;
-      
-      cout << (*top2) << " has " << (*it) << " 's childs " << endl;
+      (*top2) = str2;      
+      //cout << (*top2) << " has " << (*it) << " 's childs " << endl;
 
+      // Make tree with root nt_A. nt_A's sole child will be nt_B.
       tree<string> tree1;
       tree<string>::iterator top1 = tree1.begin();
       tree<string>::iterator one1 = tree1.insert(top1, str1);
       theTree.append_child(one1, top2);
-      cout << (*one1) << " has " << (*top2) << " as child " << endl;
+      //cout << (*one1) << " has " << (*top2) << " as child " << endl;
 
       theTree.move_ontop(it, one1);
 
-      it = one1;
+      it = one1; // go to left-most side of %%%%% rule (works for additive %%%%%-rules, e.g. nt_A%%%%%B%%%%%C)
       //system("pause");
     }
     else {
